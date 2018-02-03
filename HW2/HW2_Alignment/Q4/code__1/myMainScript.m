fdc7cc014784f5f25f9570b8558030a5439f17e3
Khursheed Ali <khursheed.ali1991@gmail.com>
2018-02-03 16:06:10 +0530

    Stiching of Multiple Images

--- HW2/HW2_Alignment/Q4/code__1/myMainScript.m
+++ HW2/HW2_Alignment/Q4/code__1/myMainScript.m
@@ -132,13 +132,11 @@ axis tight,axis on;
 o1 = get(gca, 'Position');
 colorbar(),set(gca, 'Position', o1);
 
-%%
-stich1=stichImage(timg1,timg2);
-%%
-stich2=stichImage(stich1,timg3);
+%% Stiching of Multiple Images
+stichedImage=stichMultipleImage({timg1,timg2,timg3});
 %%
 figure('name','transformed image 3');
-imshow(uint8(stich2));
+imshow(uint8(stichedImage));
 impixelinfo;
 title('\fontsize{10}{\color{magenta}transformed image 1}');
 axis tight,axis on;
