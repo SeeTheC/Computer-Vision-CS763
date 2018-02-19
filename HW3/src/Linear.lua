790246a65c9b88e0768e9562dbab83272937aac6
Anshul Gupta <anshulgupta0803@gmail.com>
2018-02-19 21:44:00 +0530

    Weight initialization and Weight updates

--- HW3/src/Linear.lua
+++ HW3/src/Linear.lua
@@ -8,43 +8,61 @@ function Linear:__init(n_input, n_output)
 	-- Input parameters and initialization
 	self.n_input = n_input
 	self.n_output = n_output
-	self.W = torch.randn(n_output, n_input)
-	self.B = torch.randn(n_output)
+	self.W = torch.randn(n_output, n_input) / math.sqrt(n_input / 2) * 0.01
+	self.B = torch.randn(n_output) / math.sqrt(n_input / 2) * 0.01
 
 	-- No need to allocate memory to these tensors
 	-- They will be replaced by the calculated values
 	self.output = torch.Tensor()
-	self.gradW = torch.Tensor()
-	self.gradB = torch.Tensor()
+	self.gradW = torch.zeros(n_output, n_input)
+	self.gradB = torch.Tensor(n_output)
 	self.gradInput = torch.Tensor()
 end
 
 function Linear:forward(input)
+	local nElement = self.output:nElement()
+	local batch_size = input:size(1)
+	self.output:resize(batch_size, self.n_output)
+	if self.output:nElement() ~= nElement then
+		self.output:zero()
+	end
 	self.output = torch.zeros(input:size(1), self.n_output)
 	for i = 1, input:size(1) do
-		self.output[i] = self.W * input[i] + self.B
+		self.output[i] = self.output[i] + self.W * input[i] + self.B
 	end
 	return self.output
 end
 
-function Linear:backward(input, gradOutput)
-	self.gradInput = gradOutput * self.W
+function Linear:backward(input, gradOutput, scale)
+	scale = scale or 1
 
-	local gradOutput_T = gradOutput:t()
-	self.gradW = gradOutput_T * input
+	local nElement = self.gradInput:nElement()
+	self.gradInput:resizeAs(input)
+	if self.gradInput:nElement() ~= nElement then
+		self.gradInput:zero()
+	end
 
-	self.gradB = torch.sum(gradOutput_T, 2)
+	self.gradInput = self.gradInput + gradOutput * self.W
 
+	self.gradW = self.gradW + scale * gradOutput:t() * input
+
+	self.gradB = self.gradB + scale * torch.sum(gradOutput:t(), 2)
+
+	self:updateParams()
 	return self.gradInput
 end
 
 function Linear:resetGrads()
-	self.gradW = torch.Tensor()
-	self.gradB = torch.Tensor()
+	self.gradW = torch.zeros(n_output, n_input)
+	self.gradB = torch.Tensor(n_output)
 	self.gradInput = torch.Tensor()
 end
 
-function Linear:updateParams(learning_rate)
-	self.W = self.W - learning_rate * self.gradW
-	self.B = self.B - learning_rate * self.gradB
+function Linear:updateParams()
+	self.W = self.W - self.gradW
+	self.B = self.B - self.gradB
+end
+
+function Linear:__tostring__()
+	return torch.type(self) .. string.format('(%d -> %d)', self.n_input, self.n_output)
 end
