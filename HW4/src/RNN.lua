b64c1b0e671fdaabbc00a6719c2f0fc4ac552961
Anshul Gupta <anshulgupta0803@gmail.com>
2018-03-16 01:52:19 +0530

    Implementing Many to One RNN layer

--- HW4/src/RNN.lua
+++ HW4/src/RNN.lua
@@ -4,77 +4,67 @@ local RNN = torch.class("RNN")
 
 function RNN:__init(inputDimension, hiddenDimension)
 	logger:debug("Initializing RNN Layer")
-	local fan_in = inputDimension + hiddenDimension
-	local fan_out = hiddenDimension
+	self.inputDimension = inputDimension
+	self.hiddenDimension = hiddenDimension
 
 	-- Xavier Initialization
-	local stdv = math.sqrt(2 / (fan_in + fan_out))
-	self.W = torch.Tensor(fan_in, fan_out):uniform(-stdv, stdv)
-	self.B = torch.Tensor(fan_out, 1):uniform(-stdv, stdv)
+	self.fan_in = self.inputDimension + self.hiddenDimension
+	self.fan_out = self.hiddenDimension
+	local stdv = math.sqrt(2.0 / (self.fan_in + self.fan_out))
+	self.W = torch.Tensor(self.fan_in, self.fan_out):uniform(-stdv, stdv)
+	self.B = torch.Tensor(self.fan_out, 1):uniform(-stdv, stdv)
 
-	self.clock = 0
-	self.timestamp = 0
-	self._output = {}
-	self._output[0] = torch.zeros(fan_out, 1)
-	self._gradW = {}
-	self._gradB = {}
-	self.output = self._output[self.timestamp]
-	self.gradW = self._gradW[self.timestamp]
-	self.gradB = self._gradB[self.timestamp]
+	self.gradientClipThreshold = 1e3
+
+	self:resetGradsAndOutputs()
+end
+
+function RNN:resetGradsAndOutputs()
+	self._hiddenOutput = {}
+	self._hiddenOutput[0] = torch.zeros(self.fan_out, 1)
+	self.output = nil
+	self.gradW = torch.zeros(self.W:size())
+	self.gradB = torch.zeros(self.B:size())
 	self.gradInput = torch.Tensor()
 end
 
 function RNN:forward(input)
-	self.clock = self.clock + 1
-	self.timestamp = self.timestamp + 1
-	-- logger:debug("Current clock: " .. self.clock)
-	-- logger:debug("Current timestamp (forward): " .. self.timestamp)
-
-	self._output[self.timestamp] = (self.W:t() * (torch.cat(self._output[self.timestamp - 1], input, 1)) + self.B):apply(math.tanh)
-	self.output = self._output[self.timestamp]
+	local size = input:size(2)
+	for i = 1, size do
+		self._hiddenOutput[i] = torch.tanh(self.W:t() * (torch.cat(self._hiddenOutput[i - 1], input:select(2, i), 1)) + self.B)
+	end
+	self.output = self._hiddenOutput[size]
 	return self.output
 end
 
-function RNN:backward(input, gradOutput, scale)
-	--[[
-			table.remove(tbl, index) pops the element from the table tbl
-			but takes the index values from 1 onwards. It can't remove
-			element at index 0
-			We are using index 0 to hold the first input to the network.
-			So when self.timestamp reaches 1, we pop the element at index 0
-			manually and copy the element at index 1 to index 0 to be used
-			in the network
-	  ]]
-	local previousInput
-	if self.timestamp > 1 then
-		x = self._output[self.timestamp - 1]
-		previousInput = table.remove(self._output, self.timestamp - 1)
-	elseif self.timestamp == 1 then
-		previousInput = self._output[0]
-		self._output[0] = table.remove(self._output, 1)
-	else
-		logger:error("Timestamp is already 0. Can't go further back.")
-		error()
+function RNN:backward(input, gradOutput)
+	self.gradW = torch.zeros(self.W:size())
+	self.gradB = torch.zeros(self.B:size())
+	local nextGradOutput = gradOutput
+	for i = input:size(2), 1, -1 do
+		local derivativeOfTanh = torch.cmul(gradOutput, 1 - torch.pow(self._hiddenOutput[i], 2))
+
+		-- Gradient Clipping
+		local gradW = torch.cat(self._hiddenOutput[i - 1], input:select(2, i), 1) * derivativeOfTanh:t()
+		local gradWNorm = torch.sum(torch.pow(gradW, 2))
+		if gradWNorm > self.gradientClipThreshold then
+			logger:info("Norm of gradient of W went above the threshold: " .. gradWNorm)
+			gradW = gradW * (self.gradientClipThreshold / gradWNorm)
+		end
+		self.gradW = self.gradW + gradW
+
+		self.gradB = self.gradB + derivativeOfTanh
+		nextGradOutput = self.W:narrow(1, 1, self.fan_out):t() * derivativeOfTanh
 	end
-
-	local derivative_of_tanh = (1 - torch.pow(previousInput, 2))
-	self._gradW[self.timestamp] = torch.cat(previousInput, input, 1) * derivative_of_tanh:t()
-	self._gradB[self.timestamp] = derivative_of_tanh
-	self.gradInput = self.W * derivative_of_tanh
-	self.gradW = self._gradW[self.timestamp]
-	self.gradB = self._gradB[self.timestamp]
-
-	self.timestamp = self.timestamp - 1
-	-- logger:debug("Current clock: " .. self.clock)
-	-- logger:debug("Current timestamp (backward): " .. self.timestamp)
+	self.gradInput = nextGradOutput
 	return self.gradInput
 end
 
 function RNN:updateParameters(learningRate)
-	for i = 1, #self._gradW do
-		self.W = self.W - learningRate * self._gradW[i]
-		self.B = self.B - learningRate * self._gradB[i]
-	end
-	self._gradW = {}
-	self._gradB = {}
+	self.W = self.W - learningRate * self.gradW
+	self.B = self.B - learningRate * self.gradB
+end
+
+function RNN:__tostring__()
+	return torch.type(self) .. string.format(': Hidden Dimension = %d and Input Dimension =  %d', self.hiddenDimension, self.inputDimension)
 end
